1798e3cd7d80a2c6c6c01a4ea7ca501bc3444f71
H2O <44346172+tonyH2O@users.noreply.github.com>
2022-06-25 21:35:15 +0300

    Add files via upload

---
+++ day4/1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int bucky[9];
+
+  cout<<"Element  -  Value"<<endl;
+
+  for(int x=0;x<9;x++)
+  {
+    cout<<x<<endl;
+    bucky[x] = 99;
+    cout<<x<<"   -----   "<<bucky[x]<<endl;
+
+  }
+}
+
+
+
---
+++ day4/2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+
+{
+  int tuna[5] = {1,2,3,4,5};
+  int sum = 0;
+
+  for(int x = 0;x<5;x++)
+  {
+    sum+=tuna[x];
+    cout<<sum<<endl;
+
+  }
+
+  }
+
+
