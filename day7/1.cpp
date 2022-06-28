e3cf2178f14c40795567f4b31cf157613ef9c0f2
H2O <44346172+tonyH2O@users.noreply.github.com>
2022-06-28 22:11:10 +0300

    Add files via upload

---
+++ day7/1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int sum = 0;
+
+  for(int val=1;val<=10;++val)
+  {
+    sum+=val;
+    cout<<sum<<endl;
+  }
+
+}
+
---
+++ day7/2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int sum=0,value=0;
+
+  while(cin>>value)
+  {
+    sum+=value;
+    
+  }
+cout<<sum<<endl;
+}
+
