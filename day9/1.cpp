ce97940aac6bf2c72a8a63bb1f979b22e5c37258
H2O <44346172+tonyH2O@users.noreply.github.com>
2022-06-30 21:52:44 +0300

    Add files via upload

---
+++ day9/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char c1,c2,c3,c4,c5;
+  cout<<"enter 5 letters: ";
+  cin>>c1>>c2>>c3>>c4>>c5;
+
+  cout<<"ASCI message: "<<int(c1)<<" "<<int(c2)<<" "<<int(c3)<<" "<<int(c4)<<" "<<int(c5)<<endl;
+}
+
+
